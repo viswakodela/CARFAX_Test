eea59dcc87b921f0df02b87c8a8d313b1275f6c7
Viswa Kodela <viswajith.kodela@ebox.ca>
2020-08-18 15:03:41 -0400

    create layout for the ListView

--- CARFAX_Test/Listings/Controller/ListingsViewController.swift
+++ CARFAX_Test/Listings/Controller/ListingsViewController.swift
@@ -13,10 +13,48 @@ class ListingsViewController: UIViewController {
     // MARK:- Properties
     private var vehicleListing      = [VehicleListViewModel]()
     
+    // MARK:- Layout Objects
+    lazy var collectionView         : UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.backgroundColor = .systemBackground
+        return cv
+    }()
+    
     // MARK:- Life cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
+        layoutView()
         fetchListings()
+    }
+    
+    // MARK:- Helpers
+    private func configureViews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func layoutView() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(1),
+                                               heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+                                                     subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
 }
 
