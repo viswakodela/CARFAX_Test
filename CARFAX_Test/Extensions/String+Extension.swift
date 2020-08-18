a362d0533adcbd440ef5ddc54c6925f1bd27cc44
Viswa Kodela <viswajith.kodela@ebox.ca>
2020-08-18 18:08:47 -0400

    improvements and code refactor

--- CARFAX_Test/Extensions/String+Extension.swift
+++ CARFAX_Test/Extensions/String+Extension.swift
@@ -13,11 +13,16 @@ extension String {
         var pureNumber = self.replacingOccurrences( of: "[^0-9]", with: "", options: .regularExpression)
         for index in 0 ..< pattern.count {
             guard index < pureNumber.count else { return pureNumber }
-            let stringIndex = String.Index(encodedOffset: index)
+            let stringIndex = String.Index(utf16Offset: index, in: pureNumber)
             let patternCharacter = pattern[stringIndex]
             guard patternCharacter != replacementCharacter else { continue }
             pureNumber.insert(patternCharacter, at: stringIndex)
         }
         return pureNumber
+    }
+}
+fileprivate extension String {
+    func indexOf(char: Character) -> Int? {
+        return firstIndex(of: char)?.utf16Offset(in: self)
     }
 }
